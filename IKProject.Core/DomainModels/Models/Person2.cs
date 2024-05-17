12a1c5656969a4977f2b4d4a5a5e72f5a560bd49
İlknur Karslı <ilknurkarsli@192.168.1.94>
2024-05-17 17:27:20 +0300

    new model added

---
+++ IKProject.Core/DomainModels/Models/Person2.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IKProject.Core.DomainModels.Models
+{
+    public class Person2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
